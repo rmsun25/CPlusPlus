613b65df2220387e865fdf274cd26c233cd92a72
rmsun25 <98938450+rmsun25@users.noreply.github.com>
2022-03-01 10:16:20 +0800

    Update RebeccaS_ClassandObject2.cpp

--- Object and Class #2/RebeccaS_ClassandObject2.cpp
+++ Object and Class #2/RebeccaS_ClassandObject2.cpp
@@ -22,11 +22,13 @@ public:
         y2 = tY2;
         x3 = tX3;
         y3 = tY3;
+        double s;
 
         side1 = sqrt((tX2-tX1)*(tX2-tX1)+(tY2-tY1)*(tY2-tY1));
-        //side2 = ;
-        //side3 = ;
-        //area =;
+        side2 = sqrt((tX3-tX2)*(tX3-tX2)+(tY3-tY2)*(tY3-tY2));
+        side3 = sqrt((tX3-tX1)*(tX3-tX1)+(tY3-tY1)*(tY3-tY1));
+        s = (side1 + side2+ side3)/2;
+        area = sqrt(s*(s-side1)*(s-side2)*(s-side3));
     }
 
 
@@ -34,5 +36,12 @@ public:
 
 int main()
 {
-
+Triangle A (0, 0, 1, 0, 0, 1);
+cout<<"First point = ("<<A.x1<<", "<<A.y1<<")\n"; 
+cout<<"Second point = ("<<A.x2<<", "<<A.y2<<")\n"; 
+cout<<"Third point = ("<<A.x3<<", "<<A.y3<<")\n"; 
+cout<<"Side 1 = "<<A.side1<<endl;
+cout<<"Side 2 = "<<A.side2<<endl;
+cout<<"Side 3 = "<<A.side3<<endl;
+cout<<"Area = "<<A.area;
 }
